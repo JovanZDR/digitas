0c8177a33c4427c08d691609407ff6d83de4c0f2
Jovan <jovan.lund@gmail.com>
2015-09-15 11:58:59 +0200

    working on the refresh button

--- instagram.js
+++ instagram.js
@@ -2,15 +2,15 @@ $(function() {
 
       var limit = 20;
       var tag = "novak";
-          
+      var id = "max_tag_id";
 
-      function getNextPage(nextId) {
+      function getNextPage(id, nextId) {
 
         $('.instGallery').html("");
         console.log("nextId",nextId);
         
         $.ajax({
-        url:"https://api.instagram.com/v1/tags/"+tag+"/media/recent?client_id=c1302f417cda4e09968eaec958fe0ae2&max_tag_id="+nextId+"",
+        url:"https://api.instagram.com/v1/tags/"+tag+"/media/recent?client_id=c1302f417cda4e09968eaec958fe0ae2&"+id+"="+nextId+"",
         dataType:"jsonp",
         type:"get",
         success: function(data) {
@@ -25,9 +25,21 @@ $(function() {
           if(maxId) {
             $(".instGallery").append("<button>nextPage</button>");
             $("button").click(function() {
-              getNextPage(maxId);
+              var id = "max_tag_id";
+              getNextPage(id, maxId);
             });
+         
+
+
           }
+           var minId = data.pagination.min_tag_id;
+          console.log("minId",minId);
+          $(".instGallery").append("<button class='refresh'>refresh page</button>");
+          $("button.refresh").click(function() {
+              var id = "min_tag_id";
+              getNextPage(id, maxId);
+            });
+
 
           console.log("info",data);
         },
@@ -50,11 +62,19 @@ $(function() {
             console.log("imgLink",imgLink);
           }
           maxId = data.pagination.next_max_id;
-          
-          $(".instGallery").append("<button>next page</button>");
-          $("button").click(function(){
-            getNextPage(maxId);
-          });
+          if (maxId) {
+            $(".instGallery").append("<button>next page</button>");
+            $("button").click(function(){
+              getNextPage(maxId);
+            });
+          }
+          var minId = data.pagination.min_tag_id;
+          console.log("minId",minId);
+          $(".instGallery").append("<button class='refresh'>refresh page</button>");
+          $("button.refresh").click(function() {
+              var id = "min_tag_id";
+              getNextPage(id, maxId);
+            });
           console.log("pagination",data.pagination);
           
           console.log("maxId",data.pagination.next_max_id);
